71cfc8af072d69adf818ece0fdd5576377b8e676
Paulo Pinto <paulo.pinto@automattic.com>
2024-10-17 10:30:47 +0100

    Boot playground

--- src/playground/Playground.tsx
+++ src/playground/Playground.tsx
@@ -1,7 +1,31 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import {
+	PlaygroundClient,
+	StartPlaygroundOptions,
+	startPlaygroundWeb,
+} from "@wp-playground/client";
 
 export function Playground(props: { siteSlug: string }) {
 	const { siteSlug } = props;
 	const iframeRef = useRef<HTMLIFrameElement>(null);
+	const [playgroundClient, setPlaygroundClient] =
+		useState<PlaygroundClient>(null);
+
+	useEffect(() => {
+		const iframe = iframeRef.current;
+		if (!iframe || playgroundClient) {
+			return;
+		}
+		async function boot() {
+			const options: StartPlaygroundOptions = {
+				iframe: iframe!,
+				remoteUrl: "https://playground.wordpress.net/remote.html",
+			};
+			const client = await startPlaygroundWeb(options);
+			setPlaygroundClient(client);
+		}
+		boot().catch(console.error);
+	}, [siteSlug, iframeRef, playgroundClient]);
+
 	return <iframe key={siteSlug} title="Playground" ref={iframeRef} />;
 }
